83e096fe59665a2d09a284fe5cece67e5ebb9e05
vivianne <viviannjoroge91@gmail.com>
2022-11-28 12:56:51 +0300

    completed assignment

--- speeddetector.js
+++ speeddetector.js
@@ -1,12 +1,15 @@
-const prompt= require("prompt-sync")();
+const prompt= require("prompt-sync")()
 var speed=prompt("Enter your speed")
-// If the speed is less than 70, it should print “Ok
+//use if conditional statements
+//if the speed is less than 70, it should print “Ok”
 if(speed<70){
-    console.log("Ok")}
-else
-{
-console.log("Points:"+((speed-70)/5));}
-//If the driver gets more than 12 points, the function should print: “License suspended”.
-if((speed-70)/5>12){
-console.log(LicenseSuspended);
+    console.log("Ok")
 }
+//if speed is not less than 70
+else{
+    console.log("Points:"+((speed-70)/5));}
+  //  If the driver gets more than 12 points, the function should print: “License suspended”.
+if((speed-70)/5>12){
+    console.log("License suspended");
+}
+
